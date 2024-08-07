1d82019b28d58299554d3e1054080e508aec6df0
Muzammil <hasanmuzammil2812@gmail.com>
2024-08-07 12:09:01 +0530

    navbar

--- src/app/Component/Navbar/navbar.js
+++ src/app/Component/Navbar/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Navbar.css"
 
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass'
+import { } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass'
 import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons'
-import { faBagShopping, faGreaterThan } from '@fortawesome/free-solid-svg-icons'
+import { faBagShopping, faGreaterThan, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Navbar() {
@@ -17,38 +18,52 @@ export default function Navbar() {
                     <img src='./Vector.png' />
                 </div>
                 <div className="navigation-search-container">
-                    <div>
-
+                    <div className='logoName'>
                         LOGO
                     </div>
-                    <div>
-                        Log
+                    <div >
+                        <ul className='navbarMenu' >
+                            <li><a href="#">SHOP</a></li>
+                            <li><a href="#">SKILLS</a></li>
+                            <li><a href="#">STORIES</a></li>
+                            <li><a href="#">ABOUT</a></li>
+                            <li><a href="#">CONTACT US</a></li>
+                        </ul>
                     </div>
                 </div>
-
+                {/* <i class="fa-sharp fa-thin fa-magnifying-glass"></i> */}
                 <div className="navigation-icons">
-
                     <a href="#" className="navigation-link">
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        {/* <i className="far fa-user-circle"></i> */}
+                        <img src='./search-normal.png' />
+                        {/* <FontAwesomeIcon icon={faMagnifyingGlass} className="fas fa-check"  style={{ color: "red", fontWeight: 200 }} /> */}
                     </a>
                     <a href="#" className="navigation-link">
-                        <FontAwesomeIcon icon={faHeart} />
-                        {/* <i className="far fa-user-circle"></i> */}
-                    </a>
+                        {/* <FontAwesomeIcon icon={faHeart} /> */}
+                        <img src='./heart.png' />
 
-                    <a href="#" className="navigation-link">
-                        <FontAwesomeIcon icon={faBagShopping} />
                         {/* <i className="far fa-user-circle"></i> */}
                     </a>
 
                     <a href="#" className="navigation-link">
-                        <FontAwesomeIcon icon={faUser} />
+                        <img src='./shopping-bag.png' />
+
+                        {/* <FontAwesomeIcon icon="fa-solid fa-coffee" size="xs" />
+                        <FontAwesomeIcon icon={faBagShopping} /> */}
                         {/* <i className="far fa-user-circle"></i> */}
                     </a>
 
                     <a href="#" className="navigation-link">
-                        <FontAwesomeIcon icon={faGreaterThan} rotation={90} />
+                        {/* <FontAwesomeIcon icon={faUser} /> */}
+                        <img src='./profile.png' />
+
+                        {/* <i className="far fa-user-circle"></i> */}
+                    </a>
+
+                    <a href="#" className="navigation-link">
+                        {/* <FontAwesomeIcon icon={faGreaterThan} rotation={90} />
+                         */}
+                        <div className='arrow-Left'>ENG  <img src='./arrow-left.png' /></div> 
+
                         {/* <i className="far fa-user-circle"></i> */}
                     </a>
 
